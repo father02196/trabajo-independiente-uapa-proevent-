84830169d84a51a96f08dfbeb6a48a503edfa8b0
father02196 <86092900+father02196@users.noreply.github.com>
2026-06-19 23:51:25 -0400

    Eliminar carpeta duplicada y agregar nuevos endpoints de Recintos y Dependencias

--- trabajo-independiente-uapa-proevent-/convert_sql.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-console.log('Reading database_backup.sql as UTF-16LE...');
-const content = fs.readFileSync('database_backup.sql', 'utf16le');
-console.log('Writing database_backup_utf8_clean.sql as UTF-8...');
-fs.writeFileSync('database_backup_utf8_clean.sql', content, 'utf8');
-console.log('Done!');
